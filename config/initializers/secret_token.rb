1af2188b519d271ca9a073dc77ad551df77bc06b
Taiki ONO <taiks.4559@gmail.com>
2013-03-27 03:06:02 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MikutterFavstar::Application.config.secret_token = 'b3a32ffa3ac4038bccf3c10b66d73eb28772ab585efa9a0c12297b9c3abeed15999268c94181bcdea5562b20ffc93d9f1006052dc444b6e0b973e59765b50cb5'
