41cef2a8e99769a4b4dead178b5621da94c72217
Taiki ONO <taiks.4559@gmail.com>
2013-03-27 03:42:39 +0900

    add login skelton

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def login?
+  end
+
+  def require_login
+    redirect_to controller: 'home', action: 'index' unless login?
+  end
 end
