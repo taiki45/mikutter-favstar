408875a603929c7a992dcecf964b99395348d06d
Taiki ONO <taiks.4559@gmail.com>
2013-04-02 22:41:42 +0900

    add destory dependent to tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :oauth_secret, :oauth_token
   attr_accessible :profile_image_url, :screen_name, :twitter_id, :updated_at
 
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
 
   def self.create_from(client, info)
     create(
