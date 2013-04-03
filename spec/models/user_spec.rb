db14fde926709e520f965775d3e795f053ea0445
Taiki ONO <taiks.4559@gmail.com>
2013-04-03 22:32:22 +0900

    add Tweet spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,22 @@ describe User do
     its(:id) { should eq 1 }
     its(:screen_name) { should eq 'taiki45' }
     its(:name) { should eq 'tai' }
+    its(:tweets) { should_not be_nil }
   end
 
+end
+
+
+describe Tweet do
+  fixtures :users, :tweets
+
+  context "belongs to taiki45" do
+    subject { User.find(1).tweets }
+
+    it { should have(2).items }
+    it "is ordered by most number" do
+      expect(subject.first.most_number).to eq 1
+      expect(subject.second.most_number).to eq 2
+    end
+  end
 end
