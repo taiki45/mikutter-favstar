0cc3cf5592db8084ee070923edf5a6c930f63503
Taiki ONO <taiks.4559@gmail.com>
2013-04-03 21:01:27 +0900

    refactoring

--- spec/models/twitter_client_spec.rb
+++ spec/models/twitter_client_spec.rb
@@ -3,10 +3,13 @@ require 'spec_helper'
 describe TwitterClient do
   fixtures :users, :tweets
 
+  let(:user) do
+    user = User.find(1)
+    user.extend described_class
+  end
+
   context "when not initialized" do
     it "should create twitter client with oauth_tokens" do
-      user = User.find(1)
-      user.extend TwitterClient
       expect(user.client).not_to be_nil
     end
   end
