592148bb2e075348faa87a75eb3269ea2866e0e8
Taiki ONO <taiks.4559@gmail.com>
2013-04-07 19:01:57 +0900

    describe Tweet's validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,7 @@ class Tweet < ActiveRecord::Base
   attr_accessible :tweet_id, :most_number
 
   belongs_to :user
+
+  validates :tweet_id, presence: true, uniqueness: true
+  validates :most_number, presence: true
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,6 +3,19 @@ require 'spec_helper'
 describe Tweet do
   fixtures :users, :tweets
 
+  context "when create new" do
+    context "with invalid data" do
+      subject { Tweet.create(tweet_id: 10) }
+
+      it { should be_invalid }
+      its(:errors) { should have(1).error }
+
+      it "should raise ActiveRecord::RecordInvalid on create!" do
+        expect { Tweet.create!(tweet_id: 10) }.to raise_error ActiveRecord::RecordInvalid
+      end
+    end
+  end
+
   context "belongs to taiki45" do
     subject { User.find(1).tweets }
 
