0f0a5fb4c46f10970f4c72cb572119dd2988ebe2
Taiki ONO <taiks.4559@gmail.com>
2013-04-11 19:46:55 +0900

    use helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  include ApplicationHelper
+  helper_method :login?, :current_user
+
+  def login?
+    session[:id] && current_user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:id])
+  end
 
   def require_login
     redirect_to controller: 'home', action: 'index' unless login?
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def login?
-    session[:id] && current_user
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:id])
-  end
 end
